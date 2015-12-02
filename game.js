6c9162bb75106750e41bf19a74b660901c41b325
WebDevStu <git@stewart-anderson.co.uk>
2015-12-02 21:26:46 +0000

    Stopping multiple clicks on the dice, until moves are finished

--- game.js
+++ game.js
@@ -4,14 +4,25 @@
     // @TODO make into class
     var game = {
         //started: false,
+        moving: false,
         dice:   new DiceClass(),
         board:  new BoardClass()
     };
 
 
+    _.listenTo({
 
-    _.listenTo('click:mouse', function () {
-        game.board.move(game.dice.throw());
+        'click:mouse': function () {
+
+            if (!game.moving) {
+                game.moving = true;
+                game.board.move(game.dice.throw());
+            }
+        },
+
+        'move:complete': function () {
+            game.moving = false;
+        }
     })
 } ());
 
